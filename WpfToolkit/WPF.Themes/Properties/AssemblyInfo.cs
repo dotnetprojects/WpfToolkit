273001218df51083a9e2edd9ab8074aa7b6584d1
jkuehner <jochen.kuehner@kardex.com>
2013-07-07 10:59:24 +0200

    WPF Themes Project

---
+++ WpfToolkit/WPF.Themes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("WPF.Themes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Impilo Software")]
+[assembly: AssemblyProduct("WPF.Themes")]
+[assembly: AssemblyCopyright("Copyright © Impilo Software 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: XmlnsDefinition("http://schemas.rudigrobler.net/wpfthemes", "WPF.Themes")]
---
+++ WpfToolkit/WPF.Themes/ThemeManager.cs
@@ -0,0 +1,115 @@
+﻿namespace WPF.Themes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    
+    public static class ThemeManager
+    {
+        public static ResourceDictionary GetThemeResourceDictionary(string theme)
+        {
+            if (theme != null)
+            {
+                Assembly assembly = Assembly.LoadFrom("WPF.Themes.dll");
+                string packUri = String.Format(@"/WPF.Themes;component/{0}/Theme.xaml", theme);
+                return Application.LoadComponent(new Uri(packUri, UriKind.Relative)) as ResourceDictionary;
+            }
+            return null;
+        }
+
+        public static string[] GetThemes()
+        {
+            string[] themes = new string[] 
+            { 
+                "ExpressionDark", "ExpressionLight", 
+                //"RainierOrange", "RainierPurple", "RainierRadialBlue", 
+                "ShinyBlue", "ShinyRed", 
+                //"ShinyDarkTeal", "ShinyDarkGreen", "ShinyDarkPurple",
+                "DavesGlossyControls", 
+                "WhistlerBlue", 
+                "BureauBlack", "BureauBlue", 
+                "BubbleCreme", 
+                "TwilightBlue",
+                "UXMusingsRed", "UXMusingsGreen", 
+                //"UXMusingsRoughRed", "UXMusingsRoughGreen", 
+                "UXMusingsBubblyBlue"
+            };
+            return themes;
+        }
+
+        public static void ApplyTheme(this Application app, string theme)
+        {
+            ResourceDictionary dictionary = ThemeManager.GetThemeResourceDictionary(theme);
+
+            if (dictionary != null)
+            {
+                app.Resources.MergedDictionaries.Clear();
+                app.Resources.MergedDictionaries.Add(dictionary);
+            }
+        }
+
+        public static void ApplyTheme(this ContentControl control, string theme)
+        {
+            ResourceDictionary dictionary = ThemeManager.GetThemeResourceDictionary(theme);
+
+            if (dictionary != null)
+            {
+                control.Resources.MergedDictionaries.Clear();
+                control.Resources.MergedDictionaries.Add(dictionary);
+            }
+        }
+
+        #region Theme
+
+        /// <summary>
+        /// Theme Attached Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty ThemeProperty =
+            DependencyProperty.RegisterAttached("Theme", typeof(string), typeof(ThemeManager),
+                new FrameworkPropertyMetadata((string)string.Empty,
+                    new PropertyChangedCallback(OnThemeChanged)));
+
+        /// <summary>
+        /// Gets the Theme property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public static string GetTheme(DependencyObject d)
+        {
+            return (string)d.GetValue(ThemeProperty);
+        }
+
+        /// <summary>
+        /// Sets the Theme property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public static void SetTheme(DependencyObject d, string value)
+        {
+            d.SetValue(ThemeProperty, value);
+        }
+
+        /// <summary>
+        /// Handles changes to the Theme property.
+        /// </summary>
+        private static void OnThemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            string theme = e.NewValue as string;
+            if (theme == string.Empty)
+                return;
+
+            ContentControl control = d as ContentControl;
+            if (control != null)
+            {
+                control.ApplyTheme(theme);
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
