de1f859d56e6716cbf9b21e02458e5677ede010e
jkuehner <jochen.kuehner@kardex.com>
2013-07-07 10:46:49 +0200

    Charting Example

---
+++ WpfToolkit/Samples/DataVisualisation/DataVisualisationSample.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace System.Windows.Controls.Samples.DataVisualisation
+{
+    /// <summary>
+    /// Interaction logic for DataVisualisationSample.xaml
+    /// </summary>
+    public partial class DataVisualisationSample : UserControl
+    {
+        public DataVisualisationSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfToolkit/Samples/DataVisualisation/SampleDataVM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace System.Windows.Controls.Samples.DataVisualisation
+{
+    public class SampleDataVM
+    {
+        public Dictionary<string,int>  Data { get; private set; }
+
+        public SampleDataVM()
+        {
+            Data = new Dictionary<string, int>();
+
+            Data.Add("aaaa", 10);
+            Data.Add("bbbb", 60);
+            Data.Add("cccc", 30);
+            Data.Add("dddd", 40);
+            Data.Add("eeee", 50);
+            Data.Add("ffff", 80);
+            Data.Add("gggg", 20);
+        }
+    }
+}
