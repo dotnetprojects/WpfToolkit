4a0b422862f282576667486dedb68091574bea13
jkuehner <jochen.kuehner@kardex.com>
2014-01-02 01:13:12 +0100

    Remove unused Designer DLLs... not needed...

--- WpfToolkit/Layout.VisualStudio.Design/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-// (c) Copyright Microsoft Corporation.
-// This source is subject to [###LICENSE_NAME###].
-// Please see [###LICENSE_LINK###] for details.
-// All other rights reserved.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Microsoft.Design", "CA1014:MarkAssembliesWithClsCompliant", Scope = "assembly", Target = "System.Windows.Controls.Layout.Toolkit.VisualStudio.Design", Justification = "Can't handle mixed references to both Silverlight and .NET.")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "System.Windows.Controls.Design.Common")]
